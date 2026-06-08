7c80e2b701e3f589f46e0c9785436ae722240a25
ricardorbs3018 <ricardorabuske@gmail.com>
2026-06-08 14:00:08 -0300

    Add files via upload
    Correcao de 404

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/my-personal-crm/sw.js', { scope: '/my-personal-crm/' })})}
